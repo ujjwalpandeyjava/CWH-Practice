0346268ee7af4264e0e725a8acfd87fe18b2b8e9
ujjwalpandeyjava <ujjwalpandey.aps@gmail.com>
2020-08-06 21:08:54 +0530

    commit

--- Express/app.js
+++ Express/app.js
@@ -4,7 +4,7 @@ const port = 80;
 
 
 app.get("/", (req, res) => {
-    res.send("This is my first express app");
+    res.send("This is my first express app");//in htlm page, we can pass whole url here
 }); 
 app.listen(port,()=>{
     console.log(`This express app has been started on port: ${port}`)
